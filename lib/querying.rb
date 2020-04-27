89929fb5b74d12e092a21e3ad09ab49340546f65
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:19:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,8 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species COUNT characters.species
   FROM characters
-  ORDER BY COUNT characters.species
+  GROUP BY characters.species
+  ORDER BY COUNT characters.species DESC
   LIMIT 1;"
 end
 
