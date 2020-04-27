36e4f0fa8be817f2157a2c83477c0656e5beffed
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:52:58 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,5 +43,6 @@ def select_character_names_and_number_of_books_they_are_in
   FROM characters
   INNER JOIN character_books
   ON character_books.character_id = characters.id
-  GROUP BY characters.name"
+  GROUP BY characters.name
+  ORDER BY characters.name"
 end
