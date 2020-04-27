da17c7fb0db3d2376f7dd1179ebb55d78292c103
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:06:34 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -5,8 +5,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT character.name, character.motto FROM character
-  ORDER BY CHAR_LENGTH(character.motto) DESC
+  "SELECT characters.name, characters.motto FROM characters
+  ORDER BY CHAR_LENGTH(characters.motto) DESC
   LIMIT 1;"
 end
 
