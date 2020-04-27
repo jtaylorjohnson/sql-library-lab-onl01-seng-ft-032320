55a6d97f7e8a6d0629384a2b351d86ea761d200a
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:10:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species.name "
+  "SELECT characters.species COUNT  "
 end
 
 def select_name_and_series_subgenres_of_authors
