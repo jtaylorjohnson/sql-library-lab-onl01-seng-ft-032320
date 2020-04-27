288a38ea0013d21884c611c82aa6f1d89f7bf894
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 17:50:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,6 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT series.title, books.year FROM series
   INNER JOIN books
   ON books.series_id = series.id 
+  WHERE 
   ORDER BY books.year;"
 end
 
