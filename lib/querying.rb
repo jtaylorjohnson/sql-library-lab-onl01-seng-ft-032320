fff961920ff13a0c63ea51e597d85ff7a393795e
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:47:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.names, COUNT(character_books)
+  "SELECT characters.name, COUNT(character_books)
   FROM characters
   INNER JOIN character_books
   ON character_books.character_id = characters.id
