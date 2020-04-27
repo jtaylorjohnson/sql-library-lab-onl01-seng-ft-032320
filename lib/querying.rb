94b8de21edc942b62e1f39fa75d2ee34af4f10b1
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:30:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,8 @@ def select_name_and_series_subgenres_of_authors
   INNER JOIN series 
   ON author.id = series.author_id
   INNER JOIN subgenres
-  ON "
+  ON series.subgenre_id = subgenres.id
+  GROUP BY authors.name"
 end
 
 def select_series_title_with_most_human_characters
