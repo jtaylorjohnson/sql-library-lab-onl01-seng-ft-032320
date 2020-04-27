2ece8e4b3afae7a2ae8959eab1a589b800771e26
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:46:21 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.names, COUNT(character_books)
   JOIN character_books
   ON character_books.character_id = characters.id
-  "
+  GROUP BY characters.name"
 end
