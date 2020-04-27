a61662518fc1fe57adec34a7dcb846331502deaa
James Johnson <jtaylorjohnson@gmail.com>
2020-04-27 18:38:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,9 +33,9 @@ def select_series_title_with_most_human_characters
   FROM series
   INNER JOIN characters
   ON series.artist_id = characters.artist_id
-  WHERE characters
-  GROUP BY 
-  "
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
